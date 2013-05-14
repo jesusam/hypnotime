c65af8cf364c501acf4645f4d3d22efd4ce7d8f1
Jesus Morales <jesusam@umich.edu>
2013-05-14 14:44:57 -0400

    Initial Commit

---
+++ HypnoTime/HypnoAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HypnoAppDelegate.h
+//  HypnoTime
+//
+//  Created by Jesus Morales on 5/14/13.
+//  Copyright (c) 2013 Jesus Morales. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HypnoAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
